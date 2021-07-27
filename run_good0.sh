c6ced2d20563294f655f0df630364e67dc60b113
root <root@osiris.intra.ispras.ru>
2021-07-27 15:54:00 +0300

    script added

---
+++ run_good0.sh
@@ -0,0 +1,9 @@
+set -x 
+
+for f in 0*; do 
+    name1="05b7a0696a5112fbced5" 
+    name2="0d4211f386d0172c4af4" 
+    if [ $f == $name1 ] || [ $f == $name2 ]; then 
+        echo "skip" continue fi echo "filename is $f" 
+        bin="`pwd`/$f" dir=`mktemp -d` (cd $dir && timeout -s KILL 3 $bin) rm -rf $dir
+done
