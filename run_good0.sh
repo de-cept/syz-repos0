739e486512cf91a43ab261ed7b55dbadf80b808b
root <root@osiris.intra.ispras.ru>
2021-07-29 10:20:43 +0300

    skript fixed

--- run_good0.sh
+++ run_good0.sh
@@ -1,9 +1,15 @@
-set -x 
+set -x
 
-for f in 0*; do 
-    name1="05b7a0696a5112fbced5" 
-    name2="0d4211f386d0172c4af4" 
-    if [ $f == $name1 ] || [ $f == $name2 ]; then 
-        echo "skip" continue fi echo "filename is $f" 
-        bin="`pwd`/$f" dir=`mktemp -d` (cd $dir && timeout -s KILL 3 $bin) rm -rf $dir
+for f in 0*; do
+    name1="05b7a0696a5112fbced5"
+    name2="0d4211f386d0172c4af4"
+    if [ $f == $name1 ] || [ $f == $name2 ]; then
+        echo "skip"
+        continue
+    fi
+    echo "filename is $f"
+    bin="`pwd`/$f"
+    dir=`mktemp -d`
+    (cd $dir && timeout -s KILL 3 $bin)
+    rm -rf $dir
 done
