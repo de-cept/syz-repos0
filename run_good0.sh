c99675008ee4be8db4348f030b5f6505772918d9
root <root@osiris.intra.ispras.ru>
2021-07-29 13:28:58 +0300

    skript fixed

--- run_good0.sh
+++ run_good0.sh
@@ -1,12 +1,6 @@
 set -x
 
 for f in *; do
-    name1="05b7a0696a5112fbced5"
-    name2="0d4211f386d0172c4af4"
-    if [ $f == $name1 ] || [ $f == $name2 ]; then
-        echo "skip"
-        continue
-    fi
     echo "filename is $f"
     bin="`pwd`/$f"
     dir=`mktemp -d`
